8605eaf7af9d8d64f75732b38d4ba814cfe5c7ea
Uzair Ahmad <134963925+uzairahm290@users.noreply.github.com>
2025-08-25 14:18:19 +0000

    Objects Completed

--- Day2.js
+++ Day2.js
@@ -1,3 +1,5 @@
+const { use } = require("react")
+
 console.log("Welcome to Day 2 !")
 
 let marvelHeros = ["thor" , "spiderman"]
@@ -19,4 +21,43 @@ let score1 = 100
 let score2 = 200
 console.log(Array.of(score1 ,score2))
 
-//
+// Singleton  object.create
+// objecty literals
+
+const mySym = Symbol("key1")
+const JsUser = {
+    name: "Uzair Ahmad",
+    location: "Lahore",
+    [mySym]: "myKey1", // it is used like this if you want to access sybmol otherwise it behaves as string => JsUser[mySym]
+    isLoggedIn: "False",
+    "Full Name": "Uzair Ahmad Makki" // it is only accessed by JsUser["Full Name"]
+}
+
+console.log(JsUser.name);
+console.log(JsUser["location"]);
+// Object.freeze(JsUser) => now below changes cannot be reflected 
+
+JsUser.greeting = function(){
+    console.log(`Hello, ${this.name}`);
+}
+
+console.log(JsUser.greeting())
+
+
+// const user = new Object() => singleton
+const user = {}
+user.name = "Uzair"
+user.rollNo = 594
+
+const user1 = {1: "a" , 2: "b"}
+
+const obj1 = {user , user1}
+const obj2 = Object.assign({}, user , obj1) // we can also use spread operator (...)
+console.log(obj2)
+
+// Object.keys() extract keys as array
+// Object.values() extract values as array
+// Object.entries() each entry as array
+
+const {name : newname} = user // now name can only be used instead of user.name
+// now the above can be accesed by newname
