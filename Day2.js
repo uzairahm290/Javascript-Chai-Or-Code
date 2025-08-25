2b3ad9e6f8b7421b51053066d6930a7a5e2e0217
Uzair Ahmad <134963925+uzairahm290@users.noreply.github.com>
2025-08-25 11:00:40 +0000

    Array Completed

--- Day2.js
+++ Day2.js
@@ -1,2 +1,22 @@
 console.log("Welcome to Day 2 !")
 
+let marvelHeros = ["thor" , "spiderman"]
+let dcHeros = ["superman", "flash"]
+const AllHeros = marvelHeros.concat(dcHeros) // retrun a singel array 
+//console.log(AllHeros)
+marvelHeros.push(dcHeros) // add array as an element in other array 
+//console.log(marvelHeros)
+
+const AllHeros1 = [...marvelHeros , ...dcHeros] // works same as concat
+// console.log(AllHeros1) 
+
+ // .flat(inifinty)  => if we have array in array in array like nested arrays so this function make its one 
+
+console.log(Array.isArray("Uzair")) // => True or false
+console.log(Array.from("Uzair")) // => converts any data type to array
+console.log(Array.isArray({name : "uzair"})) // It returns empty array => Interesting
+let score1 = 100 
+let score2 = 200
+console.log(Array.of(score1 ,score2))
+
+//
