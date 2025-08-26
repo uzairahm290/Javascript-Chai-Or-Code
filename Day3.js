ad0022a7c5eb4fcb7b22fd0180f29a222e6035a2
Uzair Ahmad <134963925+uzairahm290@users.noreply.github.com>
2025-08-26 18:29:19 +0000

    Functions Completed

--- Day3.js
+++ Day3.js
@@ -22,4 +22,53 @@ function calculate(...num1){ // rest operator => if variables are introduced bef
 }
 
 console.log(calculate(200, 300, 400)); // it returns an array
- 
+
+var c = 300 
+
+if(true){
+    let a = 10 
+    const b = 20 
+    var c = 30 
+}
+
+// console.log(a);  // it will return error
+// console.log(b);  // it will also return error 
+// console.log(c);  // it will print the answer => 30 thah the main issue in var
+
+// * Global Scope in Browser is different form the node environment
+
+// In nested funtions child can access parent variables
+
+// ##########  interesting  ##############
+console.log(addOne(5));
+
+function addOne(n){
+    return n + 1
+}
+ // the above will run even if funct is called before definition 
+
+// console.log(addTwo(5));
+
+const addTwo = function(n){
+    return n + 1
+}
+// the above will not run if it is called before definition
+
+// =>  (this) refers to the current context
+
+// Global Object in browser is Windows object
+
+function chai() {
+   //  console.log(this); it return node objects
+   let username = "Uzair"
+   console.log(this.username); // it will return undefined so it works only with objects
+   
+}
+
+const chai1 = () => {
+   // => console.log(this) // here it retunrs empty parenthesis  
+}
+
+const addTwo1 = (num1 , num2) => (num1+num2)  // now there is no need to use return and curly braces
+
+addTwo1(5,6)
