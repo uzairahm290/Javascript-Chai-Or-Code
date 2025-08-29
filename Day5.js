28f15a837ce638ed5b49f5f4c6c133a8b38737c0
Uzair Ahmad <134963925+uzairahm290@users.noreply.github.com>
2025-08-29 05:06:18 +0000

    DOM Selectors

--- Day5.js
+++ Day5.js
@@ -3,3 +3,20 @@ console.log("Welcome to Day 5");
 // We have studied about window object 
 // window.document.getElemettbyId.innerHTML 
 // how document is used in react for diferrent things
+
+// document.getElementById('title').setAttributes('class' , 'test')
+// const title = document.getElementById('title')
+// document.getElementById('title').id // .class .className
+// document.getElementById('title').getAttribute('id')
+// // title.style.backgroundColor = 'green' => we can style like this 
+
+// title.textContent // return text it returns all content if style properties are also applied
+// title.innerText // same as above but return only display content not with span properties
+// title.innerHTML // it reurns text with inner HTML Tags 
+
+// // document.getElementsByClassName('heading')
+// // document.querySelector('h2')
+// // document.querySelector('#title')
+// // document.querySelectorAll('heading') // it returns like a array
+
+// document.getElementsByClassName() // it returns HTML Collection not like Query Selector Array
